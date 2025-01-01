c2d0a501da3cb8fceab199590f1604aaa93c7708
Swayam <89599729+Swayam42@users.noreply.github.com>
2025-01-01 23:58:19 +0530

    Kth largest sum subarray

--- CodingNinjas/January-2025/Day1.java
+++ CodingNinjas/January-2025/Day1.java
@@ -1,25 +1,39 @@
-/*Given an array of integers, find the Kth largest sum subarray For example, given the array [1, -2, 3, -4, 5] and K = 2, the 2nd largest sum subarray would be [3, -4, 5], which has a sum of 4.
-
-Please note that a subarray is the sequence of consecutive elements of the array. 
-explain this to a 10yr old kid and code in java*/
+/*Given an array of integers, find the Kth largest sum subarray For example, 
+given the array [1, -2, 3, -4, 5] and K = 2, the 2nd largest sum subarray would be 
+[3, -4, 5], which has a sum of 4.
+*/
 
 import java.util.ArrayList;
 import java.util.Collections;
-public class Solution {
+public class Day1 {
 
-	public static int getKthLargest(ArrayList<Integer> arr, int k) {
-
+	public static int getKthLargest(ArrayList<Integer> arrList, int k) {
+		//an ArrayList sum to store all the sums of possible subarrays of given array 
 		ArrayList<Integer> sum = new ArrayList<>();
 
-		for(int i=0;i<arr.size();i++){
+		//
+		for(int i=0;i<arrList.size();i++){
 			int s=0;
-			for(int j=i;j<arr.size();j++)
+			for(int j=i;j<arrList.size();j++)
 			{
-				s+=arr.get(j);
+				s+=arrList.get(j);
 				sum.add(s);
 			}
 		}
+		
 		Collections.sort(sum,Collections.reverseOrder());
 		return sum.get(k-1);
+	}
+
+	public static void main(String[] args) {
+		int arr[]={1, -2, 3, -4, 5};
+		
+		//convert array to arrayList :
+		ArrayList <Integer> list=new ArrayList<Integer>();
+		for(int i:arr){
+			list.add(i);
+		}
+
+		System.out.println(getKthLargest(list, 2));
 	}
 }
